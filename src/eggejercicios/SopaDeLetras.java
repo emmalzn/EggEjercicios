6c877783a955443b15955ca6612246df6530363f
Emma Lozano <emmalzn@gmail.com>
2023-06-19 14:28:37 -0500

    Segundo commit

--- src/eggejercicios/SopaDeLetras.java
+++ src/eggejercicios/SopaDeLetras.java
@@ -10,9 +10,57 @@ de Java substring(), Length() y Math.random().
  */
 package eggejercicios;
 
+import java.util.Scanner;
+
 public class SopaDeLetras {
 
     public static void main(String[] args) {
+        char[][] sopaDeLetras = new char[20][20];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese 5 palabras de mínimo 3 y máximo 5 caracteres:");
+        String[] palabras = new String[5];
+
+        for (int i = 0; i < palabras.length; i++) {
+            palabras[i] = scanner.nextLine();
+            while (palabras[i].length() < 3 || palabras[i].length() > 5) {
+                System.out.println("La palabra debe tener entre 3 y 5 caracteres. Ingrese nuevamente:");
+                palabras[i] = scanner.nextLine();
+            }
+        }
+
+        for (int i = 0; i < palabras.length; i++) {
+            String palabra = palabras[i];
+            int filaAleatoria = (int) (Math.random() * 20);
+            int columnaInicial = (int) (Math.random() * (21 - palabra.length()));
+            System.out.println(filaAleatoria);
+            System.out.println(columnaInicial);
+            
+            for (int j = 0; j < palabra.length(); j++) {
+                sopaDeLetras[filaAleatoria][columnaInicial] = palabra.substring(j, j + 1).charAt(0);
+                columnaInicial ++; 
+            }
+            
+        }
+
+        // Rellenar los espacios no utilizados con números aleatorios del 0 al 9
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (sopaDeLetras[i][j] == 0) {
+                    sopaDeLetras[i][j] = (char) (Math.random() * 26 + 'a');
+                }
+            }
+        }
+
+        // Imprimir la sopa de letras
+        System.out.println("Sopa de Letras:");
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.print(sopaDeLetras[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 
 }
